80bc4eb8f915b8d25f9bdd6aca73d538b9546913
Tobias Fischer <info@tobiasfischer.info>
2025-06-08 13:25:12 +1000

    Add cmake cuda flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,13 @@ cp -v "${RECIPE_DIR}/FindSuiteSparse.cmake" cmake/
 
 mkdir build_ && cd build_
 
+if [[ ! -z "${cuda_compiler_version+x}" && "${cuda_compiler_version}" != "None" ]]
+  then
+    EXTRA_CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=all -DUSE_CUDA=ON"
+  else
+    EXTRA_CMAKE_ARGS="-DUSE_CUDA=OFF"
+fi
+
 cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
@@ -14,5 +21,6 @@ cmake ${CMAKE_ARGS} \
   -DBUILD_SHARED_LIBS=ON \
   -DBUILD_EXAMPLES=OFF \
   -DBUILD_TESTING=OFF \
+  ${EXTRA_CMAKE_ARGS} \
   ..
 make install -j${CPU_COUNT}
