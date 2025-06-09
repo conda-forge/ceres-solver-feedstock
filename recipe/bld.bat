e1141178443a083719fbd5da53b084ad78645b81
Tobias Fischer <info@tobiasfischer.info>
2025-06-10 00:25:03 +1000

    Add cuda build (#47)
    * Add cuda variant & bump build number
    
    * Add cmake cuda flags
    
    * Add cuda cmake args (Windows)
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.0, and conda-forge-pinning 2025.06.07.19.41.13
    
    * skip cuda 11
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.0, and conda-forge-pinning 2025.06.07.19.41.13
    
    * add libcublas-dev
    
    * add more cuda targets
    
    * fixup
    
    * cuda required at test time
    
    * libcurand not needed
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,14 +2,24 @@ COPY "%RECIPE_DIR%\FindSuiteSparse.cmake" cmake
 
 mkdir build_ && cd build_
 
+set EXTRA_CMAKE_ARGS=""
+if NOT "%cuda_compiler_version%"=="None" (
+    set EXTRA_CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=all"
+    set CUDA_ENABLED=ON
+) else (
+    set CUDA_ENABLED=OFF
+)
+
 cmake -G "NMake Makefiles" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DUSE_CUDA=%CUDA_ENABLED% ^
     -DBLA_VENDOR=Generic ^
     -DBUILD_SHARED_LIBS=ON ^
     -DBUILD_EXAMPLES=OFF ^
     -DBUILD_TESTING=OFF ^
+    %EXTRA_CMAKE_ARGS% ^
     ..
 if errorlevel 1 exit 1
 
